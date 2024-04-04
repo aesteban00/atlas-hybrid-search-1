6138b831e4a1a54496e34217ebbac524822465e5
Angel Esteban <angelesteban00@gmail.com>
2024-04-04 13:48:31 +0200

    .

--- config.mjs
+++ config.mjs
@@ -5,7 +5,7 @@ const schema = {
     titleField : "title",
     imageField : "poster",
     // vector search field
-    vectorField : "plot_embedding",
+    vectorField : "plot_embedding_hf_openai",
     // source for embedding when using embed-data.mjs
     vectorSourceField: "plot"
 }
